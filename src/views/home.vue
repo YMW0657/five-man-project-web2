66ddd81f424a4d11e8f1b526c52a7a367ffbf0aa
YMW0657 <ywan0657@student.monash.edu>
2023-09-01 00:00:50 +1000

    Initial commit

---
+++ src/views/home.vue
@@ -0,0 +1,75 @@
+
+<template>
+  <div class="index">
+    <div class="home-wrapper">
+      <div class="header">
+        <div class="logo">
+          <img src="@/assets/img/img01.png" alt="">
+        </div>
+      </div>
+      <div class="content">
+        <div class="main-page">
+          <img src="@/assets/img/img02.png" alt="">
+        </div>
+        <div class="btn-group">
+          <a-button type="primary" @click="toDashboardFn">Start Now</a-button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script setup>
+  import { useRouter } from "vue-router";
+  const router = useRouter();
+  const toDashboardFn = ()=>{
+    router.push("/dashboard");
+  }
+</script>
+<style scoped>
+  .index{
+    width:100%;
+    height: 100%;
+    position: relative;
+  }
+  .home-wrapper {
+    font-size: 20px;
+    line-height: 36px;
+  }
+  .home-wrapper .header{
+    width: 100%;
+    height: 110px;
+    background-color: #135592;
+    padding:0 30px;
+  }
+  .home-wrapper .header .logo{
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+  .home-wrapper .header .logo img{
+    width: 64px;
+  }
+  .home-wrapper .content{
+    height: calc(100vh - 110px);
+    background-color: #89aac8;
+    overflow: hidden;
+  }
+  .content .main-page img {
+    width: 80%;
+    display: block;
+    margin:30px auto;
+  }
+  .home-wrapper .content .btn-group{
+    display: flex;
+    justify-content: center;
+  }
+  .home-wrapper .content .btn-group button{
+    background-color: #135592;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  .home-wrapper .content .btn-group button:hover{
+    background-color: #1569b8;
+  }
+</style>
---
+++ src/views/scam.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="lists">
+    <div class="list" v-for="item in scams" :key="item.id">
+      <div class="logo">
+        LOGO
+      </div>
+      <div class="info">
+        <div>{{ item.title }}</div>
+        <div><a :href="item.href">{{ item.href }}</a></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import {scams} from "@/utils/index";
+</script>
+<style scoped>
+.lists{
+  padding:24px;
+}
+  .lists .list{
+    display:flex;
+    color: #222C28;
+    border-bottom: 1px solid #d9d9d9;
+    padding:12px;
+  }
+  .lists .list .logo{
+    width: 56px;
+    height: 56px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    border: 1px solid #d9d9d9;
+  }
+  .lists .list .info{
+    display: flex;
+    flex-direction: column;
+    line-height: 28px;
+    font-size: 18px;
+    justify-content: space-around;
+    margin-left: 24px;
+  }
+</style>
